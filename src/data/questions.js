3c50fb37b9d867000b89103f340f788f2f46a82c
frankhn <harfrank2@gmail.com>
2021-03-29 07:41:45 +0200

    adds an example for returning questions

---
+++ src/data/questions.js
@@ -0,0 +1,10 @@
+export const questions = [
+  {
+    question: "what is you name?",
+    answer: "My name is",
+  },
+  {
+    question: "what is school do you go to?",
+    answer: "University of Rwanda",
+  },
+];
