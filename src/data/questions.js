726225ce4768fc527b15b9d946ee733ed7e1d43a
frankhn <harfrank2@gmail.com>
2021-04-14 22:04:04 +0200

    adds id to questions

--- src/data/questions.js
+++ src/data/questions.js
@@ -1,10 +1,27 @@
 export const questions = [
   {
-    question: "what is you name?",
+    id: 1,
+    question: "1 what is you name?",
     answer: "My name is",
   },
   {
-    question: "what is school do you go to?",
+    id: 2,
+    question: " 2 what is school do you go to?",
+    answer: "University of Rwanda",
+  },
+  {
+    id: 3,
+    question: " 3 what is school do you go to?",
+    answer: "University of Rwanda",
+  },
+  {
+    id: 4,
+    question: " 4 what is school do you go to?",
+    answer: "University of Rwanda",
+  },
+  {
+    id: 5,
+    question: " 5 what is school do you go to?",
     answer: "University of Rwanda",
   },
 ];
